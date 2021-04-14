de25d39433718fd54c3fafa3026ff1afc8e95c93
Victor Muniz <victor.hm2000@aluno.ifsc.edu.br>
2021-04-14 16:53:54 -0300

    Primeiro commit

---
+++ SudokuNET/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sudokuStringOriginal = "1 3 2 5 7 9 4 6 8\n" +
+                                          "4 9 8 2 6 1 3 7 5\n" +
+                                          "7 5 6 3 8 4 2 1 9\n" +
+                                          "6 4 3 1 5 8 7 9 2\n" +
+                                          "5 2 1 7 9 3 8 4 6\n" +
+                                          "9 8 7 4 2 6 5 3 1\n" +
+                                          "2 1 4 9 3 5 6 8 7\n" +
+                                          "3 6 5 8 1 7 9 2 4\n" +
+                                          "8 7 9 6 4 2 1 5 3\n";
+
+            #region Outros Sudokus
+            string sudokuStringLinhaRepetida = "1 3 2 5 7 9 4 6 8\n" +
+                                               "4 9 8 2 5 1 3 7 5\n" +
+                                               "7 5 6 3 8 4 2 1 9\n" +
+                                               "6 4 3 1 6 8 7 9 2\n" +
+                                               "5 2 1 7 9 3 8 4 6\n" +
+                                               "9 8 7 4 2 6 5 3 1\n" +
+                                               "2 1 4 9 3 5 6 8 7\n" +
+                                               "3 6 5 8 1 7 9 2 4\n" +
+                                               "8 7 9 6 4 2 1 5 3\n";
+
+            string sudokuStringColunaRepetida = "1 3 2 5 7 9 4 6 8\n" +
+                                                "4 9 8 2 6 1 3 7 5\n" +
+                                                "8 5 6 3 7 4 2 1 9\n" +
+                                                "6 4 3 1 5 8 7 9 2\n" +
+                                                "5 2 1 7 9 3 8 4 6\n" +
+                                                "9 8 7 4 2 6 5 3 1\n" +
+                                                "2 1 4 9 3 5 6 8 7\n" +
+                                                "3 6 5 8 1 7 9 2 4\n" +
+                                                "8 7 9 6 4 2 1 5 3\n";
+
+            string sudokuStringBlocoRepetido = "1 3 2 5 7 9 4 6 8\n" +
+                                               "4 9 8 2 6 1 3 7 5\n" +
+                                               "7 5 6 3 8 4 2 1 9\n" +
+                                               "6 4 3 1 5 8 7 9 2\n" +
+                                               "5 2 1 7 9 3 8 4 6\n" +
+                                               "9 8 7 4 2 6 5 3 1\n" +
+                                               "2 1 4 9 3 5 6 8 7\n" +
+                                               "3 6 5 8 1 7 9 2 4\n" +
+                                               "8 7 2 6 4 9 1 5 3\n";
+            #endregion
+
+            int[,] sudokuMatriz = StringParaMatriz(sudokuStringOriginal);
+
+            //if (ValidarSudoku(sudoku))
+            if (VerificarBlocos(sudokuMatriz))
+            {
+                Console.WriteLine("SIM");
+            }
+            else
+            {
+                Console.WriteLine("NAO");
+            }
+            Console.ReadLine();
+        }
+
+        private static int[,] StringParaMatriz(string sudokuString)
+        {
+            int[,] sudokuMatriz = new int[9, 9];
+
+            sudokuString = sudokuString.Replace("\n", " ");
+            string[] aux = sudokuString.Split(' ');
+
+            int count = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    sudokuMatriz[i, j] = Convert.ToInt32(aux[count]);
+                    count++;
+                }
+            }
+
+            return sudokuMatriz;
+        }
+
+        private static bool VerificarRepeticao(int[] arr)
+        {
+            Hashtable tabelaVerificacao = new Hashtable();
+
+            foreach (int i in arr)
+            {
+                if (tabelaVerificacao.ContainsKey(i))
+                    return true;
+                else
+                    tabelaVerificacao.Add(i, i);
+            }
+
+            return false;
+        }
+
+        #region Verificação de linhas
+        private static int[] LinhaParaArray(int i, int[,] sudokuMatriz)
+        {
+            int[] linha = new int[9];
+            int count = 0;
+
+            for (int j = 0; j < 9; j++)
+            {
+                linha[count] = sudokuMatriz[i, j];
+                count++;
+            }
+
+            return linha;
+        }
+
+        private static bool VerificarLinhas(int[,] sudokuMatriz)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                int[] linha = LinhaParaArray(i, sudokuMatriz);
+
+                if (VerificarRepeticao(linha))
+                    return false;
+            }
+
+            return true;
+        }
+        #endregion
+
+        #region Verificação de colunas
+        private static int[] ColunaParaArray(int j, int[,] sudokuMatriz)
+        {
+            int[] coluna = new int[9];
+            int count = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                coluna[count] = sudokuMatriz[i, j];
+                count++;
+            }
+
+            return coluna;
+        }
+
+        private static bool VerificarColunas(int[,] sudokuMatriz)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                int[] coluna = ColunaParaArray(j, sudokuMatriz);
+
+                if (VerificarRepeticao(coluna))
+                    return false;
+            }
+
+            return true;
+        }
+        #endregion
+
+        #region Verificação de blocos
+        private static int[] BlocoParaArray(int iB, int jB, int[,] sudokuMatriz)
+        {
+            int[] bloco = new int[9];
+            int count = 0;
+            for (int i = 0; i < 9; i += 3)
+            {
+                for (int j = 0; j < 9; j += 3)
+                {
+                    for (int ii = 0; ii < 3; ii++)
+                    {
+                        for (int jj = 0; jj < 3; jj++)
+                        {
+                            bloco[count] = sudokuMatriz[i, j];
+                            Console.Write(bloco[count]);
+                            count++;
+                        }
+                        count = 0;
+                    }
+                }
+            }
+            Console.WriteLine();
+            return bloco;
+        }
+
+        private static bool VerificarBlocos(int[,] sudokuMatriz)
+        {
+            for (int iB = 0; iB < 9; iB += 3)
+            {
+                for (int jB = 0; jB < 9; jB += 3)
+                {
+                    int[] bloco = BlocoParaArray(iB, jB, sudokuMatriz);
+
+                    if (VerificarRepeticao(bloco))
+                        return false;
+                }
+            }
+
+            return true;
+        }
+        #endregion
+    }
+}
